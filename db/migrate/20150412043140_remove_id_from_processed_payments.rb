ccb9db3503c8b8f2810f2553b663a124fd9dfa16
Pedro Scocco <pedroscocco@gmail.com>
2015-04-12 09:52:44 -0300

    Fixed heroku migration

--- db/migrate/20150412043140_remove_id_from_processed_payments.rb
+++ db/migrate/20150412043140_remove_id_from_processed_payments.rb
@@ -2,7 +2,7 @@ class RemoveIdFromProcessedPayments < ActiveRecord::Migration
   def change
     change_table :processed_payments do |t|
       t.change :payment_id, :string, null: false
-      t.remove :id, :string
+      t.remove :id
     end
   end
 end
